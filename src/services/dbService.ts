dca12752ea800552776861ff12714998c646b066
FebriPutraPetir <fpp255@gmail.com>
2025-09-05 16:08:57 +0700

    Komit 0004 nft backend

--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -1,7 +1,17 @@
-import mongoose from "mongoose";
+ import mongoose from "mongoose";
+
+// export async function connectDB() {
+//   const uri = process.env.MONGO_URI;
+//   await mongoose.connect(uri);
+//   console.log("MongoDB connected:", uri);
+// }
+
 
 export async function connectDB() {
   const uri = process.env.MONGO_URI;
+  if (!uri) {
+    throw new Error("process.env.MONGO_URI is undefined. Check your .env file");
+  }
   await mongoose.connect(uri);
-  console.log("✅ MongoDB connected:", uri);
+  console.log("MongoDB connected:", uri);
 }
